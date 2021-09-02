52f8646c4da553346163d58261d0b5fd81b65f23
Brady Shannon <brady.shannon@my.simpson.edu>
2021-09-02 15:01:05 -0500

    Work on testing.

---
+++ Testing/test.py
@@ -0,0 +1,11 @@
+print("Most exciting class ever!! Woo!")
+print("What am I most excited for?")
+print("What is an \"Escape code\" ?")
+print("How do I print a backslash? \\ ")
+print("""Holy cow!
+The 
+Professor 
+has 
+gone 
+crazy!
+""")
