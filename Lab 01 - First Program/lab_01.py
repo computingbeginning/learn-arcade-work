25527b616c085a3eb013825cd8b293c925f82e75
Brady Shannon <brady.shannon@my.simpson.edu>
2021-09-02 15:21:42 -0500

    Talk to professor

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,10 @@
+print("I am using my own sentences!")
+print("I should probably tell you I have taken computer programming classes in high school.")
+print("Still, taking this class should be helpful for me.")
+print("""I should also tell you I have ADD, or
+Attention 
+Deficit 
+Disorder.
+""")
+print("I space out easily.")
+print("Even so, I will try and do my best in this class!")
