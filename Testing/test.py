c42fe11ffae4c36f2397e15eec19721608391990
Brady Shannon <brady.shannon@my.simpson.edu>
2021-09-20 12:30:10 -0500

    Worked on if and while loops. Quiz and labs due soon.

--- Testing/test.py
+++ Testing/test.py
@@ -1,10 +1,4 @@
-temperature = int(input("What is the temperature in Fahrenheit? "))
-if temperature > 110:
-    print("Oh man, you could fry eggs on the pavement!")
-elif temperature > 90:
-    print("It is hot outside")
-elif temperature < 30:
-    print("It is cold outside")
-else:
-    print("It is ok outside")
-print("Done")
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
