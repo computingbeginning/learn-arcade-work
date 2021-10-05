12480d2994bddd2e6ce2be37d4fa12bbcd12f0b9
Brady Shannon <brady.shannon@my.simpson.edu>
2021-10-05 10:49:39 -0500

    Quiz is done, new lab starts tomorrow

--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,4 @@
-my_list = [-4, -2, -56, -2, -30]
-positive_outlook_list = []
-biggest_number = my_list[0]
-for item in my_list:
-    if item >= 0:
-        positive_outlook_list.append(item)
-
-print(positive_outlook_list)
+for i in range(3):
+    for j in range(3):
+        print("*", end="")
+print()
