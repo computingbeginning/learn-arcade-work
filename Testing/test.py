1e7cc2f675242a5cd8b70c1790f52255a2e60c1a
Brady Shannon <brady.shannon@my.simpson.edu>
2021-10-06 12:25:51 -0500

    Learned about clases.

--- Testing/test.py
+++ Testing/test.py
@@ -1,4 +1,15 @@
-for i in range(3):
-    for j in range(3):
-        print("*", end="")
-print()
+class Address:
+    def __init__(self,
+                 name: str = "",
+                 line1: str = "",
+                 line2: str = "",
+                 city: str = "",
+                 state: str = "",
+                 zip_code: str = ""
+                 ):
+        self.name: str = name
+        self.line1: str = line1
+        self.line2: str = line2
+        self.city: str = city
+        self.state: str = state
+        self.zip_code: str = zip_code
