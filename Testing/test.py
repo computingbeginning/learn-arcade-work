5f99465b7eed3287e5c7a0ee8adfb12f873e203a
Brady Shannon <brady.shannon@my.simpson.edu>
2021-10-25 12:30:32 -0500

    did stuff

--- Testing/test.py
+++ Testing/test.py
@@ -2,6 +2,7 @@
 
 import random
 import arcade
+import math
 
 # --- Constants ---
 SPRITE_SCALING_PLAYER = 0.5
@@ -13,11 +14,36 @@ SCREEN_HEIGHT = 600
 
 
 class Coin(arcade.Sprite):
+
+    def __init__(self, filename, sprite_scaling):
+        """ Constructor. """
+        # Call the parent class (Sprite) constructor
+        super().__init__(filename, sprite_scaling)
+
+        # Current angle in radians
+        self.circle_angle = 0
+
+        # How far away from the center to orbit, in pixels
+        self.circle_radius = 0
+
+        # How fast to orbit, in radians per frame
+        self.circle_speed = 0.008
+
+        # Set the center of the point we will orbit around
+        self.circle_center_x = random.randrange(SCREEN_WIDTH)
+        self.circle_center_y = random.randrange(SCREEN_HEIGHT)
+
     def update(self):
-        self.center_y -= 1
-        if self.top < 0:
-            self.bottom = random.randrange(SCREEN_HEIGHT, SCREEN_HEIGHT * 2)
-            self.center_x = random.randrange(SCREEN_WIDTH)
+
+        """ Update the ball's position. """
+        # Calculate a new x, y
+        self.center_x = self.circle_radius * math.sin(self.circle_angle) \
+            + self.circle_center_x
+        self.center_y = self.circle_radius * math.cos(self.circle_angle) \
+            + self.circle_center_y
+
+        # Increase the angle in prep for the next round.
+        self.circle_angle += self.circle_speed
 
 
 class MyGame(arcade.Window):
@@ -52,6 +78,14 @@ class MyGame(arcade.Window):
             coin = Coin("coin_01.png", SPRITE_SCALING_COIN)
             coin.center_x = random.randrange(SCREEN_WIDTH)
             coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Random radius from 10 to 200
+            coin.circle_radius = random.randrange(10, 200)
+
+            # Random start angle from 0 to 2pi
+            coin.circle_angle = random.random() * 2 * math.pi
+
+            # Add the coin to the lists
             self.coin_list.append(coin)
 
     def on_draw(self):
