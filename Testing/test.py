509fe13c7bf7be7c521fcdd0f840f32a1d6ff646
Brady Shannon <brady.shannon@my.simpson.edu>
2021-11-19 12:23:11 -0600

    Started sorting work

--- Testing/test.py
+++ Testing/test.py
@@ -1,75 +1,35 @@
-import arcade
+"""
+I should probably loop through the whole thing
+"""
 
-WIDTH = 60
-HEIGHT = 60
-MARGIN = 5
+# my_list = [15, 57, 14, 33, 72, 79, 26, 56, 42, 40]
+# my_list = [14, 57, 15, 33, 72, 79, 26, 56, 42, 40]
+# my_list = [14, 15, 57, 33, 72, 79, 26, 56, 42, 40]
+# my_list = [14, 15, 26, 33, 72, 79, 57, 56, 42, 40]
+# my_list = [14, 15, 26, 33, 72, 79, 57, 56, 42, 40]
+# my_list = [14, 15, 26, 33, 40, 79, 57, 56, 42, 72]
+# my_list = [14, 15, 26, 33, 40, 42, 57, 56, 79, 72]
+# my_list = [14, 15, 26, 33, 40, 42, 56, 57, 79, 72]
+# my_list = [14, 15, 26, 33, 40, 42, 56, 57, 79, 72]
+# my_list = [14, 15, 26, 33, 40, 42, 56, 57, 72, 79]
 
-ROW_COUNT = 10
-COLUMN_COUNT = 10
-
-SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
-SCREEN_HEIGHT = (HEIGHT + MARGIN) * COLUMN_COUNT + MARGIN
-
-
-class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
-
-    def __init__(self, width, height):
-        super().__init__(width, height)
-
-        arcade.set_background_color(arcade.color.BLACK)
-
-        self.grid = []
-
-        for row in range(ROW_COUNT):
-            self.grid.append([])
-            for column in range(COLUMN_COUNT):
-                self.grid[row].append(0)
-
-        self.grid[3][4] = 1
-
-    def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        arcade.start_render()
-
-        color = 0
-
-        for row in range(ROW_COUNT):
-            for column in range(COLUMN_COUNT):
-                x = (WIDTH / 2) + column * (WIDTH + MARGIN) + MARGIN
-                y = HEIGHT / 2 + row * (HEIGHT + MARGIN) + MARGIN
-                if self.grid[row][column] == 0:
-                    color = arcade.color.LIGHT_CYAN
-                elif self.grid[row][column] == 1:
-                    color = arcade.color.LIGHT_GREEN
-
-                arcade.draw_rectangle_filled(x, y,
-                                             WIDTH, HEIGHT,
-                                             color)
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        row = y // (HEIGHT + MARGIN)
-        column = x // (WIDTH + MARGIN)
-        print("click", row, column)
-        if self.grid[row][column] == 1:
-            self.grid[row][column] = 0
-        elif self.grid[row][column] == 0:
-            self.grid[row][column] = 1
+# This is selection sort. Selecting the smallest number and swapping.
 
 
-def main():
+def selection_sort(my_list):
+    for cur_pos in range(len(my_list)):
+        min_pos = cur_pos
+        for scan_pos in range(cur_pos + 1, len(my_list)):
+            if my_list[scan_pos] < my_list[min_pos]:
+                min_pos = scan_pos
+        temp = my_list[min_pos]
+        my_list[min_pos] = my_list[cur_pos]
+        my_list[cur_pos] = temp
 
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
-    arcade.run()
 
+my_list = [15, 57, 14, 33, 72, 79, 26, 56, 42, 40]
+selection_sort(my_list)
+print(my_list)
 
-if __name__ == "__main__":
-    main()
+# n = 10, 10 * 5 = 50
+# n = 100, 100 * 50 = 5000
